2058a233a83d1a7a46229c26431bd9c4da8bf105
shagu <ericmauser@googlemail.com>
2025-07-21 09:15:09 +0200

    create new dragon frame that holds both textures

--- pfUI-eliteoverlay.lua
+++ pfUI-eliteoverlay.lua
@@ -34,7 +34,6 @@ pfUI:RegisterModule("EliteOverlay", "vanilla:tbc", function ()
 
   pfUI:UpdateConfig("EliteOverlay",       nil,         "position",   "right")
 
-
   local HookRefreshUnit = pfUI.uf.RefreshUnit
   function pfUI.uf:RefreshUnit(unit, component)
     local pos = string.upper(C.EliteOverlay.position)
@@ -44,56 +43,52 @@ pfUI:RegisterModule("EliteOverlay", "vanilla:tbc", function ()
     local size = unit:GetWidth() / 1.5
     local elite = UnitClassification(unitstr)
 
-    unit.dragonTop = unit.dragonTop or unit:CreateTexture(nil, "OVERLAY")
-    unit.dragonBottom = unit.dragonBottom or unit:CreateTexture(nil, "OVERLAY")
+    unit.dragon = unit.dragon or CreateFrame("Frame", nil, unit)
+    unit.dragon:SetAllPoints(unit)
+    unit.dragon:SetFrameLevel(16)
+
+    unit.dragonTop = unit.dragonTop or unit.dragon:CreateTexture(nil, "OVERLAY")
+    unit.dragonBottom = unit.dragonBottom or unit.dragon:CreateTexture(nil, "OVERLAY")
 
     if unitstr == "" or C.EliteOverlay.position == "off" then
-      unit.dragonTop:Hide()
-      unit.dragonBottom:Hide()
+      unit.dragon:Hide()
     else
       unit.dragonTop:ClearAllPoints()
       unit.dragonTop:SetWidth(size)
       unit.dragonTop:SetHeight(size)
       unit.dragonTop:SetPoint("TOP"..pos, unit, "TOP"..pos, invert*size/5, size/7)
-      unit.dragonTop:SetParent(unit.hp.bar)
 
       unit.dragonBottom:ClearAllPoints()
       unit.dragonBottom:SetWidth(size)
       unit.dragonBottom:SetHeight(size)
       unit.dragonBottom:SetPoint("BOTTOM"..pos, unit, "BOTTOM"..pos, invert*size/5.2, -size/2.98)
-      unit.dragonBottom:SetParent(unit.hp.bar)
 
       if elite == "worldboss" then
         unit.dragonTop:SetTexture(addonpath.."\\img\\TOP_GOLD_"..pos)
-        unit.dragonTop:Show()
         unit.dragonTop:SetVertexColor(.85,.15,.15,1)
         unit.dragonBottom:SetTexture(addonpath.."\\img\\BOTTOM_GOLD_"..pos)
-        unit.dragonBottom:Show()
         unit.dragonBottom:SetVertexColor(.85,.15,.15,1)
+        unit.dragon:Show()
       elseif elite == "rareelite" then
         unit.dragonTop:SetTexture(addonpath.."\\img\\TOP_GOLD_"..pos)
-        unit.dragonTop:Show()
         unit.dragonTop:SetVertexColor(1,1,1,1)
         unit.dragonBottom:SetTexture(addonpath.."\\img\\BOTTOM_GOLD_"..pos)
-        unit.dragonBottom:Show()
         unit.dragonBottom:SetVertexColor(1,1,1,1)
+        unit.dragon:Show()
       elseif elite == "elite" then
         unit.dragonTop:SetTexture(addonpath.."\\img\\TOP_GOLD_"..pos)
-        unit.dragonTop:Show()
         unit.dragonTop:SetVertexColor(.75,.6,0,1)
         unit.dragonBottom:SetTexture(addonpath.."\\img\\BOTTOM_GOLD_"..pos)
-        unit.dragonBottom:Show()
         unit.dragonBottom:SetVertexColor(.75,.6,0,1)
+        unit.dragon:Show()
       elseif elite == "rare" then
         unit.dragonTop:SetTexture(addonpath.."\\img\\TOP_GRAY_"..pos)
-        unit.dragonTop:Show()
         unit.dragonTop:SetVertexColor(.8,.8,.8,1)
         unit.dragonBottom:SetTexture(addonpath.."\\img\\BOTTOM_GRAY_"..pos)
-        unit.dragonBottom:Show()
         unit.dragonBottom:SetVertexColor(.8,.8,.8,1)
+        unit.dragon:Show()
       else
-        unit.dragonTop:Hide()
-        unit.dragonBottom:Hide()
+        unit.dragon:Hide()
       end
     end
 
